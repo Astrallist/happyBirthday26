f6be8d1cf380cf14b22a2f901e16d3832cef8b9f
Astrallist <132389598+Astrallist@users.noreply.github.com>
2025-06-01 01:16:44 +0300

    Delete src directory

--- src/invitedUsers.js
+++
@@ -1,8 +0,0 @@
-export const invitedUsers = [
-    { firstName: "Максим", lastName: "Москалев", sex: "мужской" },
-    { firstName: "Евгений", lastName: "Гоцев", sex: "мужской" },
-    { firstName: "Ася", lastName: "Шарова", sex: "женский" },
-    { firstName: "Дария", lastName: "Которова", sex: "женский" },
-    { firstName: "Анастасия", lastName: "Сизова", sex: "женский" },
-    { firstName: "Анастасия", lastName: "Абрамова", sex: "женский" },
-];
